48976acb52d0be3be036d525b66eedf6cb107eb9
breno505 <brenomurilo2099@gmail.com>
2023-12-15 14:40:58 -0300

    feat(javascript.js): add getComputerChoice(), function to generate the computer's choice (rock, paper, or scissors)

---
+++ javascript.js
@@ -0,0 +1,6 @@
+// Function to generate the computer's choice (rock, paper, or scissors)
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
